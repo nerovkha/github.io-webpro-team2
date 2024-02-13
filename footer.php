9d1b2ab1013d0aff593999335b089fa760189c5c
zwekhantlwin <zwekhantlwin5@gmail.com>
2024-02-13 17:13:02 +0200

    editing footer

--- footer.php
+++ footer.php
@@ -2,6 +2,12 @@
         <div class="container container-fluid text-center copyright">
             © Joo-Marketti
         </div>
+        <?php
+    $filename = basename($_SERVER['PHP_SELF']);
+    if (file_exists($filename)) {
+        echo "<div>Last modified: " . date("F d, Y H:i:s", filemtime($filename)) . "</div>";
+    }
+    ?>
     </div>
 
     <!-- Bootstrap JS and dependencies -->
